35073fe9a19221e48bc488facd01fdba4e7ce3b1
silverchris <silverchris@gmail.com>
2022-04-28 11:23:03 -0400

    Updated openssl

--- scripts/10-openssl.sh
+++ scripts/10-openssl.sh
@@ -1,11 +1,16 @@
 source "$(dirname "$0")/settings.sh"
 
-if [ ! -f "openssl-1.1.1h.tar.gz" ]; then
-    wget -q https://www.openssl.org/source/openssl-1.1.1h.tar.gz
+git clone https://github.com/cryptodev-linux/cryptodev-linux.git
+install -m 644 -D cryptodev-linux/crypto/cryptodev.h "$SYSROOT"/usr/include/crypto/cryptodev.h
+
+if [ ! -f "openssl-3.0.2.tar.gz" ]; then
+    wget -q https://www.openssl.org/source/openssl-3.0.2.tar.gz
 fi
 
-tar xvf openssl-1.1.1h.tar.gz
-cd openssl-1.1.1h || exit
-./Configure linux-armv4 -march=armv7-a --cross-compile-prefix=arm-mazda-linux-musleabi- --prefix="$SYSROOT"/usr
-make
-make install
+tar xvf openssl-3.0.2.tar.gz
+cd openssl-3.0.2 || exit
+./Configure linux-armv4 no-ssl3 no-ssl2 no-idea no-dtls no-psk no-srp no-weak-ssl-ciphers no-shared enable-devcryptoeng -static -march=armv7-a --cross-compile-prefix=arm-mazda-linux-musleabi- -mtune=cortex-a9 -mfpu=neon -fexpensive-optimizations -frename-registers -fomit-frame-pointer --prefix="$SYSROOT"/usr
+#./Configure linux-armv4 no-ssl3 no-ssl2 no-idea no-dtls no-psk no-srp no-weak-ssl-ciphers no-shared -static -march=armv7-a --cross-compile-prefix=arm-mazda-linux-musleabi- -mtune=cortex-a9 -mfpu=neon -fexpensive-optimizations -frename-registers -fomit-frame-pointer --prefix="$SYSROOT"/usr
+
+make -j`nproc`
+make install_sw
